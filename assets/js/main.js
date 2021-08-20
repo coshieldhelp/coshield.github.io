145cf07ae1948c4f43053cf69c98ded102bab73a
coshieldhelp <coshieldhelp@gmail.com>
2021-08-20 12:55:07 +0530

    updated 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,16 @@
 !(function($) {
   "use strict";
 
+
+ // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(40).fadeOut('fast', function() {
+        $(this).remove();
+      });
+    }
+  });
+
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
